4b7cb5d47a639f9f4ff70b2c0b4daae37aa1f4d1
Ethan <ethancredo.ascc@outlook.com>
2026-01-06 14:12:44 +0800

    add chicken tinola to menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,10 +1,17 @@
 import menu1 from "../img/sagit.jpg";
+import chickenTinola from "../img/chicken-tinola.jpg";
 
 const menu = [
     {
-        name: "Menu 1",
-        description: "Menu description",
+        name: "Chicken Tinola",
+        description: "Chicken Tinola is a Filipino soup dish. It involves cooking chicken pieces in ginger broth. It is a popular cold weather dish. I always feel warm and cozy every time I eat it. This Filipino Chicken Soup is best enjoyed with fish sauce as dipping sauce, and a cup of warm white rice.",
         price: "Php 200",
+        url: chickenTinola,
+    },
+    {
+        name: "Menu 2",
+        description: "Menu2 description",
+        price: "Php 400",
         url: menu1,
     },
 ];
@@ -21,15 +28,26 @@ export const renderMenu = () => {
 
     menu.forEach(item => {
         const card = document.createElement("div");
+        const menuTitle = document.createElement("div");
+        const img = document.createElement("img");
         const name = document.createElement("h3");
         const price = document.createElement("div");
         const description = document.createElement("p");
 
+        card.classList.add("card");
+        img.classList.add("img");
+        menuTitle.classList.add("menu-title");
+        name.classList.add("name");
+        price.classList.add("price");
+        description.classList.add("description");
+
+        img.src = item.url;
         name.textContent = item.name;
         price.textContent = item.price;
         description.textContent = item.description;
 
-        card.append(name, price, description);
+        menuTitle.append(name, price);
+        card.append(img, menuTitle, description);
         menuContainer.append(card);
     });
 
