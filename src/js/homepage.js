44def4204fb6d04027dc53ba0fb5fa12274d44ed
Ethan <ethancredo.ascc@outlook.com>
2026-01-06 11:56:45 +0800

    add welcome and opening hours

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -1,10 +1,26 @@
-const content = document.getElementById("content");
-const h2 = document.createElement("h2");
-
 export const homepage = () => {
+    const content = document.getElementById("content");
+    const welcomeContainer = document.createElement("div");
+    const welcome = document.createElement("h2");
+    const welcomePara = document.createElement("p");
+    const openingHoursContainer = document.createElement("div");
+    const openingHours = document.createElement("h2");
+    const openingPara = document.createElement("p");
+
     content.textContent = "";
 
-    h2.textContent = "Welcome!";
+    welcome.textContent = "Welcome to Sagi's Carenderia!";
+    welcomePara.textContent = "Kaon namo dri kay lami kayo ni!";
+    welcomeContainer.append(welcome);
+    welcomeContainer.append(welcomePara);
+    welcomeContainer.classList.add("welcome");
+    content.append(welcomeContainer);
 
-    content.append(h2);
+    openingHours.textContent = "Opening Hours";
+    openingPara.textContent = "We are open from Monday to Saturday 7am to 8pm."
+    openingHoursContainer.append(openingHours);
+    openingHoursContainer.append(openingPara);
+    openingHoursContainer.classList.add("opening-hours");
+    content.append(openingHoursContainer);
+    
 }
