c6344c5b3bb0d148e25037ba6818ba83531eb4f2
Ethan <ethancredo.ascc@outlook.com>
2026-01-07 15:21:55 +0800

    add nav button active css

--- src/index.js
+++ src/index.js
@@ -8,9 +8,25 @@ const menuBtn = document.getElementById("menu-btn");
 const aboutBtn = document.getElementById("about-btn");
 
 renderHomepage();
+homeBtn.classList.add("active");
 
-homeBtn.addEventListener("click", () => renderHomepage());
-menuBtn.addEventListener("click", () => renderMenu());
-aboutBtn.addEventListener("click", () => renderAbout());
+homeBtn.addEventListener("click", () => {
+    homeBtn.classList.add("active");
+    menuBtn.classList.remove("active");
+    aboutBtn.classList.remove("active");
+    renderHomepage();
+});
+menuBtn.addEventListener("click", () => {
+    homeBtn.classList.remove("active");
+    menuBtn.classList.add("active");
+    aboutBtn.classList.remove("active");
+    renderMenu();
+});
+aboutBtn.addEventListener("click", () => {
+    homeBtn.classList.remove("active");
+    menuBtn.classList.remove("active");
+    aboutBtn.classList.add("active");
+    renderAbout();
+});
 
 console.log("Hello World!");
